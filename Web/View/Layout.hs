05334068b63b8b33ae8a8bfbd172d315fddaec56
Marc Scholten <marc@digitallyinduced.com>
2020-10-06 17:46:02 +0200

    Click on IHP Forum logo should go to start page

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -27,7 +27,7 @@ defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
 <body>
     <div class="container mt-4">
         <nav class="navbar navbar-expand-lg navbar-light mb-4">
-            <a class="navbar-brand" href="#">λ IHP Forum</a>
+            <a class="navbar-brand" href="/">λ IHP Forum</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
