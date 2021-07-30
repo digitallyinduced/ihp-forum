c61a5fca6c8db30376e0d094ca7eef9f21a43350
Marc Scholten <marc@digitallyinduced.com>
2021-07-30 08:43:37 +0200

    Updated IHP Version

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -124,7 +124,7 @@ scripts :: Html
 scripts = do
     when isDevelopment [hsx|
         <script id="livereload-script" src="/livereload.js"></script>
-        <script src="/vendor/jquery-3.2.1.slim.min.js"></script>
+        <script src="/vendor/jquery-3.6.0.slim.min.js"></script>
         <script src="/vendor/timeago.js"></script>
         <script src="/vendor/popper.min.js"></script>
         <script src="/vendor/bootstrap.min.js"></script>
