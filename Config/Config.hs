f4d85039be61c6fea922692ea0475531dbc928d2
Marc Scholten <marc@digitallyinduced.com>
2026-04-13 12:43:08 +0200

    Qualify LoginSupport.Middleware import to avoid authMiddleware clash
    The bare authMiddleware from IHP.LoginSupport.Middleware clashes with
    the authMiddleware record field on FrameworkConfig (added in
    digitallyinduced/ihp#2259). Use a qualified import to disambiguate.

--- Config/Config.hs
+++ Config/Config.hs
@@ -3,14 +3,14 @@ module Config where
 import IHP.Prelude
 import IHP.Environment
 import IHP.FrameworkConfig
-import IHP.LoginSupport.Middleware (authMiddleware, adminAuthMiddleware)
+import qualified IHP.LoginSupport.Middleware as Auth
 import Generated.Types
 
 config :: ConfigBuilder
 config = do
     -- See https://ihp.digitallyinduced.com/Guide/config.html
     -- for what you can do here
-    option $ AuthMiddleware (authMiddleware @User . adminAuthMiddleware @Admin)
+    option $ AuthMiddleware (Auth.authMiddleware @User . Auth.adminAuthMiddleware @Admin)
 
 slackWebHook :: Maybe String
 slackWebHook = Nothing
